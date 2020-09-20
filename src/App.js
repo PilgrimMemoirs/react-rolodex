e7576fbd06462d17f0f24da7d7df98f19ca15504
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2020-09-19 20:33:25 -0700

    add font and gradient background

--- src/App.js
+++ src/App.js
@@ -11,12 +11,17 @@ class App extends Component {
       contacts: [],
       searchField: ''
     };
+
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ contacts: users }))
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
   }
   
   render() {
@@ -26,9 +31,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1> Rolodex </h1>
         <SearchBox 
           placeholder="Search Contacts"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList contacts={filteredContacts} />
       </div>
